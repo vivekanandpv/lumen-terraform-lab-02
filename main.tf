b2bc5892aecd0eec7983a6b2e60977196dbb8f98
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-16 15:04:14 +0530

    working copy

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
