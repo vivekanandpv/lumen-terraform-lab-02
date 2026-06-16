4cd0187db2bbbcba29667b8010336d507787992e
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-16 14:39:34 +0530

    added outputs

--- main.tf
+++ main.tf
@@ -125,3 +125,15 @@ resource "aws_instance" "server" {
 }
 
 
+output "instance_public_ip" {
+  value = aws_instance.server.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.server.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
